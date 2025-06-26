39edc36f1471982e65e2bad4ee5a62088e8c24af
Md Nehal Akhlaque <123729545+mdnehal01@users.noreply.github.com>
2025-06-26 20:52:27 +0530

    Updated convex-provider.tsx

--- lib/providers/convex-provider.tsx
+++ lib/providers/convex-provider.tsx
@@ -1,28 +1,3 @@
-// "use client"
-// import { ReactNode } from "react";
-// import { ConvexReactClient } from "convex/react";
-// import { ConvexProviderWithClerk } from "convex/react-clerk";
-// import { ClerkProvider, useAuth } from "@clerk/nextjs";
-
-// const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
-// export const ConvexClientProvider = ({
-//     children
-// }:{
-//     children: ReactNode;
-// }) => {
-//   return(
-//     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-//         <ConvexProviderWithClerk
-//             useAuth={useAuth}
-//             client={convex}
-//         >
-//             {children}
-//         </ConvexProviderWithClerk>
-//     </ClerkProvider>
-//   )
-// }
-
 'use client'
 
 import { ReactNode } from 'react'
@@ -30,18 +5,25 @@ import { ConvexReactClient } from 'convex/react'
 import { ConvexProviderWithClerk } from 'convex/react-clerk'
 import { ClerkProvider, useAuth } from '@clerk/nextjs'
 
-if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL
+
+if (!convexUrl) {
   throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
 }
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL)
+const convex = new ConvexReactClient(convexUrl)
 
 export default function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
-        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-            {children}
-        </ConvexProviderWithClerk>
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        baseTheme: undefined,
+      }}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
     </ClerkProvider>
   )
 }
