461e89dab8c14f86fa3bc25f45ffc33077e4ae1c
Md Nehal Akhlaque <123729545+mdnehal01@users.noreply.github.com>
2025-07-01 22:02:20 +0530

    Updated workspace-navigation.tsx

--- app/(main)/(routes)/workspace/[workspaceId]/_components/workspace-navigation.tsx
+++ app/(main)/(routes)/workspace/[workspaceId]/_components/workspace-navigation.tsx
@@ -175,10 +175,6 @@ const WorkspaceNavigation = ({ workspace }: WorkspaceNavigationProps) => {
                 </div>
 
                 <div>
-                    {/* <UserItem/> */}
-
-                  <h1>HHHHHDASKJB</h1>
-                    
                     {/* Workspace Header */}
                     <div className="px-3 py-2 border-b border-theme-lightgreen/20">
                         <div className="flex items-center gap-2 mb-2">
