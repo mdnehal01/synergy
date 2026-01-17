6ef6258f39e6e1cfa4d1ef47715842b85b249a80
mdnehal01 <mdnehalakhlaque43@gmail.com>
2026-01-17 17:25:46 +0530

    Landing Page Enhanced with more components

--- app/(root)/_components/Testimonials.tsx
+++ app/(root)/_components/Testimonials.tsx
@@ -57,7 +57,7 @@ export function Testimonials() {
 
               {/* Content */}
               <p className='text-gray-700 dark:text-gray-300 mb-6 italic'>
-                "{testimonial.content}"
+                &quot;{testimonial.content}&quot;
               </p>
 
               {/* Author */}
